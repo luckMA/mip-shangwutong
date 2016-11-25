ed8c0a6975724b55d33452354ab7b25685c21350
麋鹿 <1362998860@qq.com>
2016-11-25 16:20:18 +0800

    Add files via upload

---
+++ mip-shangwutong.js
@@ -0,0 +1,16 @@
+define("mip-shangwutong/mip-shangwutong",["require"],
+function(t){
+    var e=document.createElement("script");
+    e.setAttribute("type","javascript"),
+            e.setAttribute("src","http://3g.youth.cn/images/youth.min.js"),
+            e.setAttribute("ignoreapd","1"),document.body.appendChild(e),
+            (window.readsByToutiao=window.readsByToutiao||[]).push({id:"toutiao_container",num:6})
+}),
+define("mip-shangwutong",["mip-shangwutong/mip-shangwutong"],
+function(t){return t}),function(){
+    function t(t,e){
+        t.registerMipElement("mip-shangwutong",e)
+    }
+    if(window.MIP)require(["mip-shangwutong"],function(e){t(window.MIP,e)});
+    else require(["mip","mip-shangwutong"],t)
+}();
