cf7cd043105809d881e20d5e9e08030a2cedbf45
麋鹿 <1362998860@qq.com>
2016-11-26 14:17:54 +0800

    Update mip-shangwutong.js

--- mip-shangwutong.js
+++ mip-shangwutong.js
@@ -3,8 +3,7 @@ function(t){
     var e=document.createElement("script");
     e.setAttribute("type","javascript"),
             e.setAttribute("src","http://zhongliu.yunyu51.com/15/swt/36683/jhdx.js"),
-            e.setAttribute("ignoreapd","1"),document.body.appendChild(e),
-            (window.readsByToutiao=window.readsByToutiao||[]).push({id:"toutiao_container",num:6})
+            e.setAttribute("ignoreapd","1"),document.body.appendChild(e)
 }),
 define("mip-shangwutong",["mip-shangwutong/mip-shangwutong"],
 function(t){return t}),function(){
