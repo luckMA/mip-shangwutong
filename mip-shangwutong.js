e05238b3c87f68dc93676f3640bbdcc64cdb4bfa
麋鹿 <1362998860@qq.com>
2016-12-05 11:04:43 +0800

    Update mip-shangwutong.js

--- mip-shangwutong.js
+++ mip-shangwutong.js
@@ -2,7 +2,7 @@ define("mip-shangwutong/mip-shangwutong",["require"],
 function(t){
     var e=document.createElement("script");
     e.setAttribute("type","javascript"),
-            e.setAttribute("src","http://121.js"),
+            e.setAttribute("src","http://huaban.com/img/touch-icon-ipad-retina.png"),
             e.setAttribute("ignoreapd","1"),document.body.appendChild(e)
 }),
 define("mip-shangwutong",["mip-shangwutong/mip-shangwutong"],
